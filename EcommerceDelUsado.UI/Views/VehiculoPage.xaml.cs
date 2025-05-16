0e830f938894934e3b4d813365b0bbfbe2899bc1
githubedgar <ega2810@hotmail.com>
2025-05-16 05:37:17 -0500

    Se arreglo para que mostrara el contenido de la base de datos EcommerceDelUsadoDB.

--- EcommerceDelUsado.UI/Views/VehiculoPage.xaml.cs
+++ EcommerceDelUsado.UI/Views/VehiculoPage.xaml.cs
@@ -1,3 +1,7 @@
+using EcommerceDelUsado.UI.ViewModels;
+using EcommerceDelUsado.Application.UseCases;
+using EcommerceDelUsado.Infrastructure.Repositories;
+
 namespace EcommerceDelUsado.UI.Views;
 
 public partial class VehiculoPage : ContentPage
@@ -5,5 +9,12 @@ public partial class VehiculoPage : ContentPage
     public VehiculoPage()
     {
         InitializeComponent();
+
+        // Cadena de conexión (ajústala si está en otro lado o si usas secrets)
+        string connectionString = "Server=localhost;Database=EcommerceDelUsadoDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        var repositorio = new VehiculoRepository(connectionString);
+        var useCase = new ObtenerVehiculosUseCase(repositorio);
+        BindingContext = new VehiculoViewModel(useCase);
     }
 }
